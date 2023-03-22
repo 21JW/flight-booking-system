fc2743dd3c4c2d26ce06ccce6ec65859b13a125f
JR <z8ruan@uwaterloo.ca>
2023-03-22 16:09:04 -0400

    add Flight

--- src/main/java/com/example/mybatis_practice_user/Service/Impl/FlightServiceImpl.java
+++ src/main/java/com/example/mybatis_practice_user/Service/Impl/FlightServiceImpl.java
@@ -1,11 +1,27 @@
 package com.example.mybatis_practice_user.Service.Impl;
 
 import com.example.mybatis_practice_user.Service.FlightService;
+import com.example.mybatis_practice_user.mapper.FlightMapper;
 import com.example.mybatis_practice_user.model.dto.FlightDTO;
+import com.example.mybatis_practice_user.model.entity.Flight;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
+@Service
+@Slf4j
 public class FlightServiceImpl implements FlightService {
+
+    @Autowired
+    FlightMapper flightMapper;
+
     @Override
     public void addFlight(FlightDTO dto){
-
+        Flight flight=new Flight(dto);
+        flight.setCreateTime(new Date());
+        flight.setTs(new Date());
+        flightMapper.insert(flight);
     }
 }
--- src/main/java/com/example/mybatis_practice_user/controller/FlightController.java
+++ src/main/java/com/example/mybatis_practice_user/controller/FlightController.java
@@ -1,4 +1,28 @@
 package com.example.mybatis_practice_user.controller;
 
+import com.example.mybatis_practice_user.Service.FlightService;
+import com.example.mybatis_practice_user.model.dto.FlightDTO;
+import com.example.mybatis_practice_user.model.dto.UserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/flight")
 public class FlightController {
+    private FlightService flightService;
+    @Autowired
+    public FlightController(FlightService flightService){
+        this.flightService=flightService;
+    }
+    @PostMapping
+    public ResponseEntity<?> Register(@RequestBody @Validated(FlightDTO.Insert.class) FlightDTO dto) {
+        flightService.addFlight(dto);
+        return new ResponseEntity(HttpStatus.OK);
+    }
 }
